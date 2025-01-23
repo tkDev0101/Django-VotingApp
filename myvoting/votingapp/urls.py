9a6822364909bc73ecb65a2653b4f2a4c1959b77
tkDev <tkdev0101@gmail.com>
2025-01-23 22:03:40 +0200

    Added templates

--- myvoting/votingapp/urls.py
+++ myvoting/votingapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('', views)
+    path('', views.index, name="index"),
+    path('vote', views.detail, name="detail"),
+    path('result', views.result, name="result"),
+    path('signin', views.signin, name="signin"),
+    path('signup', views.signup, name="signup"),
 ]
